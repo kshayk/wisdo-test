50917fc7a268ce442641e369f17e97857ff24db0
kshayk <kshayk0@gmail.com>
2024-01-07 15:10:37 +0200

    Fixed test expected results

--- src/test/post.test.ts
+++ src/test/post.test.ts
@@ -3,11 +3,11 @@ import {rateAlgo} from "../service/feedService";
 describe('rate posts', () => {
     test('rate post 1', async () => {
         const rating = rateAlgo(0, 453);
-        expect(rating).toBe<number>(300);
+        expect(rating).toBe<number>(0);
     });
 
     test('rate post 2', async () => {
         const rating = rateAlgo(13, 110);
-        expect(rating).toBe<number>(400);
+        expect(rating).toBe<number>(10);
     });
 });
